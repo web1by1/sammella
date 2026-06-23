26d2893fdb1f1bbf46feb86b72c2aac22d3e2a50
mava <mavafiza@gmail.com>
2026-06-23 08:06:13 +0200

    Ajustes de despliegue

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router"; // ← Cambiado
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     Component: Home,
@@ -12,3 +12,19 @@ export const router = createBrowserRouter([
     Component: Detail,
   },
 ]);
+
+
+// import { createBrowserRouter } from "react-router";
+// import Home from "./pages/Home";
+// import Detail from "./pages/Detail";
+
+// export const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     Component: Home,
+//   },
+//   {
+//     path: "/card/:slug",
+//     Component: Detail,
+//   },
+// ]);
--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: '/sammella/', 
   resolve: {
     alias: {
       // Alias @ to the src directory
